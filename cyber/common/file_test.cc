1d491b4940913494fe7175489bc71ad306724b50
hhkblogi <hhkb.logi@gmail.com>
2023-03-19 18:34:28 +0000

    Add a testcase for SetProto() and GetProto()

--- cyber/common/file_test.cc
+++ cyber/common/file_test.cc
@@ -29,28 +29,28 @@ namespace apollo {
 namespace cyber {
 namespace common {
 
-// TEST(FileTest, proto_set_get_test) {
-//   apollo::cyber::proto::UnitTest message;
-//   message.set_class_name("FileTest");
-//   apollo::cyber::proto::UnitTest read_message;
-//   EXPECT_FALSE(SetProtoToASCIIFile(message, -1));
-//   EXPECT_FALSE(SetProtoToASCIIFile(message, "not_exists_dir/message.proto"));
-//   EXPECT_TRUE(SetProtoToASCIIFile(message, "message.ascii"));
-//   EXPECT_TRUE(SetProtoToBinaryFile(message, "message.binary"));
+TEST(FileTest, proto_set_get_test) {
+  apollo::cyber::proto::UnitTest message;
+  message.set_class_name("FileTest");
+  apollo::cyber::proto::UnitTest read_message;
+  EXPECT_FALSE(SetProtoToASCIIFile(message, -1));
+  EXPECT_FALSE(SetProtoToASCIIFile(message, "not_exists_dir/message.proto"));
+  EXPECT_TRUE(SetProtoToASCIIFile(message, "message.ascii"));
+  EXPECT_TRUE(SetProtoToBinaryFile(message, "message.binary"));
 
-//   EXPECT_FALSE(
-//       GetProtoFromASCIIFile("not_exists_dir/message.proto", &read_message));
-//   EXPECT_FALSE(
-//       GetProtoFromBinaryFile("not_exists_dir/message.proto", &read_message));
-//   EXPECT_TRUE(GetProtoFromASCIIFile("message.ascii", &read_message));
-//   EXPECT_TRUE(GetProtoFromBinaryFile("message.binary", &read_message));
+  EXPECT_FALSE(
+      GetProtoFromASCIIFile("not_exists_dir/message.proto", &read_message));
+  EXPECT_FALSE(
+      GetProtoFromBinaryFile("not_exists_dir/message.proto", &read_message));
+  EXPECT_TRUE(GetProtoFromASCIIFile("message.ascii", &read_message));
+  EXPECT_TRUE(GetProtoFromBinaryFile("message.binary", &read_message));
 
-//   EXPECT_FALSE(GetProtoFromFile("not_exists_dir/message.proto", &read_message));
-//   EXPECT_TRUE(GetProtoFromFile("message.binary", &read_message));
+  EXPECT_FALSE(GetProtoFromFile("not_exists_dir/message.proto", &read_message));
+  EXPECT_TRUE(GetProtoFromFile("message.binary", &read_message));
 
-//   remove("message.binary");
-//   remove("message.ascii");
-// }
+  remove("message.binary");
+  remove("message.ascii");
+}
 
 // TEST(FileTest, file_utils_test) {
 //   apollo::cyber::proto::UnitTest message;
